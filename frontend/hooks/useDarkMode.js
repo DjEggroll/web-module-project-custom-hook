aa17b3fdaf020f207056a4d7a7d2fe81fa9ad34c
Jiahui Feng <feng_jeffrey@yahoo.com>
2022-06-08 17:00:29 -0400

    Dark Mode hook created. Replaced darkmode state in App

---
+++ frontend/hooks/useDarkMode.js
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+
+
+const useDarkMode = (initialValue) => {
+    const [darkMode, setDarkMode] = useState(initialValue)
+
+    return([darkMode, setDarkMode]);
+}
+
+export default useDarkMode;
